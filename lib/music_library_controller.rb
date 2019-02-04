8ee31f99994212b6c4e15180e00cfd2f4547ef5f
Kelly Sheddy <jungaka07@gmail.com>
2019-02-04 13:31:46 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,7 +38,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
+    Song.all.sort_by(&:name).each.with_index(1) do |song, index - 1|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
