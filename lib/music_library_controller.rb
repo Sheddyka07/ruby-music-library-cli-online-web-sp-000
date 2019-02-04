aba65ac94195da9a09489b3f6eb522990f278d5f
Kelly Sheddy <jungaka07@gmail.com>
2019-02-04 13:27:59 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,15 +1,12 @@
 class MusicLibraryController
 
-  extend Concerns::Findable
-
-  def initialize(path = "./db/mp3s")
-    new_importer_object = MusicImporter.new(path)
-    new_importer_object.import
+  def initialize(path = './db/mp3s')
+    @path = MusicImporter.new(path)
+    @path.import
   end
 
   def call
-    input = ""
-    while input != "exit"
+    user_input = ''
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -19,23 +16,24 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      input = gets.strip
 
-      case input
-      when "list songs"
+    until user_input == "exit"
+      user_input = gets.chomp
+
+
+       if user_input == "list songs"
         list_songs
-      when "list artists"
+      elsif user_input == "list artists"
         list_artists
-      when "list genres"
+      elsif user_input == "list genres"
         list_genres
-      when "list artist"
+      elsif user_input == "list artist"
         list_songs_by_artist
-      when "list genre"
+      elsif user_input == "list genre"
         list_songs_by_genre
-      when "play song"
+      elsif user_input == "play song"
         play_song
       end
-
     end
   end
 
@@ -45,71 +43,54 @@ class MusicLibraryController
     end
   end
 
-
   def list_artists
-    #prints all artists in the music library in a numbered list (alphabetized by artist name
-    songs_sorted_by_artist = Artist.all.sort_by do |artist| #returns an array of artists sorted by artist name
-      artist.name
-    end
-    songs_sorted_by_artist.each.with_index(1) do |artist,index|
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
       puts "#{index}. #{artist.name}"
     end
   end
 
   def list_genres
-    #prints all genres in the music library in a numbered list (alphabetized by genre name)
-    songs_sorted_by_genre = Genre.all.sort_by do |genre|
-      genre.name
-    end
-    songs_sorted_by_genre.each.with_index(1) do |genre,index|
+    Genre.all.sort_by(&:name).each.with_index(1) do |genre, index|
       puts "#{index}. #{genre.name}"
     end
+
   end
 
-
   def list_songs_by_artist
-    #prints all songs by a particular artist in a numbered list (alphabetized by song name)
     puts "Please enter the name of an artist:"
-    input = gets.chomp
-    if artist = Artist.find_by_name(input) #find artist that matches input
-      #get the list of songs and collect a new list that is alphabetized by song name
-      songs_sorted_by_name = artist.songs.sort_by do |song|
-        song.name
-      end
-      #print the each item in the list
-      songs_sorted_by_name.each.with_index(1) do |song,index|
-        puts "#{index}. #{song.name} - #{song.genre.name}"
+    user_input = gets.strip
+
+    if artist = Artist.find_by_name(user_input)
+      artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
+      puts "#{index}. #{song.name} - #{song.genre.name}"
       end
     end
   end
 
   def list_songs_by_genre
-    #prints all songs by a particular genre in a numbered list (alphabetized by song name)
     puts "Please enter the name of a genre:"
-    input = gets.chomp
-    if genre = Genre.find_by_name(input) #find genre that matches input
-      #get the list of songs and collect a new list that is alphabetized by song name
-      songs_sorted_by_name = genre.songs.sort_by do |song|
-        song.name
-      end
-      #print the each item in he list
-      songs_sorted_by_name.each.with_index(1) do |song,index|
-        puts "#{index}. #{song.artist.name} - #{song.name}"
+    user_input = gets.strip
+
+    if genre = Genre.find_by_name(user_input)
+      genre.songs.sort_by(&:name).each.with_index(1) do |song, index|
+      puts "#{index}. #{song.artist.name} - #{song.name}"
       end
     end
   end
 
   def play_song
     puts "Which song number would you like to play?"
-    list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+    user_input = gets.strip.to_i
 
-    input = gets.strip.to_i
-    #if (input > 0) && (input <= list_of_songs.size)
-    #if (input >= 1) && (input <= list_of_songs.size)
-    if (1..Song.all.length).include?(input)
-      song = list_of_songs[input+2]
-      puts "Playing #{song.name} by #{song.artist.name}"
+    Song.all.sort_by(&:name).each_with_index do |song, index|
+      if (index + 1) == user_input
+        puts "Playing #{song.name} by #{song.artist.name}"
+      end
     end
   end
+
+
+
+
 
 end
